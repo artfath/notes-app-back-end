4a5844fd04360917b9641985f2185f719b80b1ad
fath <fatahilamuhammad@gmail.com>
2023-02-13 11:22:50 +0700

    changes port

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT || 4000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes:{
         cors:{
             origin:['*'],
